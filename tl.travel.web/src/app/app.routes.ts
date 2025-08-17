8507a43ca321f10011333a02d2c7c135be425137
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2025-08-17 16:58:04 +0300

    Add reservations

--- tl.travel.web/src/app/app.routes.ts
+++ tl.travel.web/src/app/app.routes.ts
@@ -47,6 +47,10 @@ export const routes: Routes = [
       {
         path: 'payment-types',
         loadComponent: () => import('./components/payment-types/payment-types.component').then(m => m.PaymentTypesComponent)
+      },
+      {
+        path: 'reservations',
+        loadComponent: () => import('./components/reservations/reservations.component').then(m => m.ReservationsComponent)
       }
     ]
   }
