311f36bfe0b21c2b3dd15d3ada95ca97e6c4a341
Ivaylo Stoyanov <ipstoyanov20@codingburgas.bg>
2025-08-17 16:45:03 +0300

    Make changes and deletions

--- tl.travel.web/src/app/app.routes.ts
+++ tl.travel.web/src/app/app.routes.ts
@@ -43,6 +43,10 @@ export const routes: Routes = [
       {
         path: 'tour-operators',
         loadComponent: () => import('./components/tour-operators/tour-operators.component').then(m => m.TourOperatorsComponent)
+      },
+      {
+        path: 'payment-types',
+        loadComponent: () => import('./components/payment-types/payment-types.component').then(m => m.PaymentTypesComponent)
       }
     ]
   }
---
+++ tl.travel.web/src/app/models/reservation-module.ts
@@ -0,0 +1,11 @@
+export class EditReservationDTO {
+ id: string;
+ startDate: Date;
+ endDate: Date;
+ clientId: string;
+ roomId: string;
+
+
+
+
+ }
